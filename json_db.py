194eefce6979826546a4cd3d90e3b0bac08efcf5
Nwosu David <snjhauser@gmail.com>
2024-09-10 20:19:27 +0100

    read json file fxn

--- json_db.py
+++ json_db.py
@@ -4,12 +4,19 @@ import os
 class JSONDB:
     def __init__(self):
         self.cwd = os.getcwd()
-        self.filepath = 'tracker.json'
+        self.filename = 'tracker.json'
+        self.filepath = os.path.join(self.cwd, self.filename)
         self.json = self.read_db()
 
     def read_db(self):
-        with open(os.path.join(self.cwd, self.filepath), 'a') as f:
-            f.write('[]')
+        if not os.path.exists(self.filepath):
+            return 'No task to do!'
+        with open(self.filepath) as f:
+            return f.read()
+    
+    def write_db(self):
+        ...
 
 
 me = JSONDB()
+print(me.json)
